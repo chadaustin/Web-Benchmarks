efffa47359ecc31d4b45945f4af9f01b3baf48fc
Chad Austin <caustin@gmail.com>
2014-04-26 15:36:40 -0700

    a few more binary string representations

--- meminit/meminit.py
+++ meminit/meminit.py
@@ -1,8 +1,26 @@
 import json
+import base64
 
-data = map(chr, json.load(file('meminit.json', 'rb')))
+data = ''.join(map(chr, json.load(file('meminit.json', 'rb'))))
+print 'data length:', len(data)
 
+def tinystr(s):
+    s = repr(s)
+    for i in range(8):
+        s = s.replace('\\x0' + str(i), '\\' + str(i))
+    return s
 
-file('meminit.json.binary', 'wb').write(''.join(data))
-file('meminit.json.str', 'wb').write(repr(''.join(data)))
+file('meminit.json.binary', 'wb').write(data)
+file('meminit.json.str', 'wb').write(repr(data))
+file('meminit.json.minstr', 'wb').write(tinystr(data))
+file('meminit.json.base64', 'wb').write(repr(base64.b64encode(data)))
 
+if 0 == len(data) % 4:
+    out = []
+    for i in range(0, len(data), 4):
+        a = data[i]
+        b = data[i+1]
+        c = data[i+2]
+        d = data[i+3]
+        out.append(ord(a) | (ord(b) << 8) | (ord(c) << 16) | (ord(d) << 24))
+    file('meminit.json.int32', 'wb').write('[' + ','.join(map(str, out)) + ']')
