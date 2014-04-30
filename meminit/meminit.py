952b30d58668d068d219311ec67a596ed3e5de4b
Chad Austin <chad@chadaustin.me>
2014-04-29 22:58:41 -0700

    some timing benchmarks for int8 vs. a minified string literal

--- meminit/meminit.py
+++ meminit/meminit.py
@@ -1,22 +1,39 @@
+# thanks to evanw for his improvements https://gist.github.com/evanw/11339324
+
+import os
+import re
 import json
 import base64
-
-data = ''.join(map(chr, json.load(file('meminit.json', 'rb'))))
+ 
+path = 'meminit.json'
+data = ''.join(map(chr, json.loads(open(path, 'rb').read())))
 print 'data length:', len(data)
-
+ 
 def tinystr(s):
+    hex_to_octal = lambda x: '\\%s' % (oct(int(x.group(1), 16))[1:] or 0)
     s = repr(s)
-    for i in range(8):
-        s = s.replace('\\x0' + str(i), '\\' + str(i))
-    for i in range(8, 10):
-        s = s.replace('\\x0' + str(i), '\\1' + str(i - 8))
+    s = re.sub(r'\\x([0-1][0-9A-Fa-f])(?:(?=[^0-9])|$)', hex_to_octal, s)
     return s
-
-file('meminit.json.binary', 'wb').write(data)
-file('meminit.json.str', 'wb').write(repr(data))
-file('meminit.json.minstr', 'wb').write(tinystr(data))
-file('meminit.json.base64', 'wb').write(repr(base64.b64encode(data)))
-
+ 
+def size(bytes):
+    return '%.1f KB' % (bytes / 1000.0)
+ 
+def write(type, data):
+    output = path + '.' + type
+    file(output, 'wb').write(data)
+    os.system('gzip --stdout --best %s > %s.gz' % (output, output))
+    gzip = file(output + '.gz', 'rb').read()
+    print '| %14s | %17s | %12s |' % (type, size(len(data)), size(len(gzip)))
+ 
+print '| Representation | uncompressed size | gzipped size |'
+print '| -------------- | ----------------- | ------------ |'
+ 
+write('binary', data)
+write('minstr', tinystr(data))
+write('str', repr(data))
+write('int8', '[' + ','.join(map(str, map(ord, data))) + ']')
+write('base64', repr(base64.b64encode(data)))
+ 
 if 0 == len(data) % 4:
     out = []
     for i in range(0, len(data), 4):
@@ -25,4 +42,4 @@ if 0 == len(data) % 4:
         c = data[i+2]
         d = data[i+3]
         out.append(ord(a) | (ord(b) << 8) | (ord(c) << 16) | (ord(d) << 24))
-    file('meminit.json.int32', 'wb').write('[' + ','.join(map(str, out)) + ']')
+    write('int32', '[' + ','.join(map(str, out)) + ']')
